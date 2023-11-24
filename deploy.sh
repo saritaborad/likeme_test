e9a20e6b05f131a812e507b9f0f1396ee1dc66d4
saritaborad <borad.sarita@gmail.com>
2023-11-24 18:07:27 +0530

    code changes

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Set the deployment directory
-deploy_dir="root@piks.in:/var/www/piks/public_html/likeme_test"
+# Set the deployment directory on the server
+deploy_dir="piks.in:/var/www/piks/public_html/likeme_test"
 
 # Save the 'uploads' folder path
 uploads_folder="$deploy_dir/backend/uploads"
@@ -9,40 +9,26 @@ uploads_folder="$deploy_dir/backend/uploads"
 # Check if the 'uploads' folder exists on the server
 if [ -d "$uploads_folder" ]; then
   # If it exists, move it to a temporary location
-  mv "$uploads_folder" "$deploy_dir/uploads_temp"
+  ssh $deploy_dir "mv $uploads_folder $deploy_dir/uploads_temp"
 fi
-
-# Navigate to the repository root
-cd $deploy_dir
 
 # Pull the latest changes from the remote repository (assuming the branch is 'master')
 git pull origin master
 
-# Navigate to the backend directory
-cd backend
+# Navigate to the backend directory on the server
+ssh $deploy_dir "cd backend && npm install && pm2 restart likeme_test"
 
-# Install Node.js dependencies
-npm install
+# Navigate to the frontend directory on the server
+ssh $deploy_dir "cd frontend && npm install && npm run build"
 
-# Restart your Node.js server (assuming you are using pm2)
-pm2 restart likeme_test
-
-# Navigate to the frontend directory
-cd ../frontend
-
-# Install frontend dependencies and build the React app
-npm install
-npm run build
-
-
-# Copy the build files to the deployment directory
-cp -R build/* $deploy_dir
+# Copy the build files to the deployment directory on the server
+scp -r build/* $deploy_dir
 
 # Check if the 'uploads' folder was moved to a temporary location
 if [ -d "$deploy_dir/uploads_temp" ]; then
   # If it was moved, restore it to its original location
-  mv "$deploy_dir/uploads_temp" "$uploads_folder"
+  ssh $deploy_dir "mv $deploy_dir/uploads_temp $uploads_folder"
 fi
 
-# Optional: Remove the frontend directory if you no longer need it
-rm -r $deploy_dir/frontend
+# Optional: Remove the frontend directory locally if you no longer need it
+# rm -r /path/to/your/local/repository/frontend
