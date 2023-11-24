2239284667dfd263662d783b6d3c134d00ece879
saritaborad <borad.sarita@gmail.com>
2023-11-24 14:30:39 +0530

    code chaneges

--- deploy.sh
+++ deploy.sh
@@ -3,27 +3,46 @@
 # Set the deployment directory
 deploy_dir="/var/www/piks/public_html/likeme_test"
 
-# Navigate to the backend directory
-cd $deploy_dir/backend
+# Save the 'uploads' folder path
+uploads_folder="$deploy_dir/backend/uploads"
 
-# Pull the latest changes from the Git repository
+# Check if the 'uploads' folder exists on the server
+if [ -d "$uploads_folder" ]; then
+  # If it exists, move it to a temporary location
+  mv "$uploads_folder" "$deploy_dir/uploads_temp"
+fi
+
+# Navigate to the repository root
+cd $deploy_dir
+
+# Pull the latest changes from the remote repository (assuming the branch is 'master')
 git pull origin master
+
+# Navigate to the backend directory
+cd backend
 
 # Install Node.js dependencies
 npm install
 
-# Restart your Node.js server
+# Restart your Node.js server (assuming you are using pm2)
 pm2 restart likeme_test
 
 # Navigate to the frontend directory
-cd $deploy_dir/frontend
+cd ../frontend
 
-# Pull the latest changes from the Git repository
-git pull origin master
-
-# Build the React app
+# Install frontend dependencies and build the React app
 npm install
 npm run build
 
-# Reload Apache (if needed)
-sudo service apache2 reload
+
+# Copy the build files to the deployment directory
+cp -R build/ $deploy_dir
+
+# Check if the 'uploads' folder was moved to a temporary location
+if [ -d "$deploy_dir/uploads_temp" ]; then
+  # If it was moved, restore it to its original location
+  mv "$deploy_dir/uploads_temp" "$uploads_folder"
+fi
+
+# Optional: Remove the frontend directory if you no longer need it
+rm -r $deploy_dir/frontend
