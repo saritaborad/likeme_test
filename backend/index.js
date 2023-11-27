59ba9bc75a50703b5bd45e3ad7f7ef046108de7b
saritaborad <borad.sarita@gmail.com>
2023-11-27 16:12:42 +0530

    code changes

--- backend/index.js
+++ backend/index.js
@@ -18,16 +18,11 @@ let origin = process.env.PRODUCTION == 1 ? process.env.REACT_LIVE_URL : process.
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use(cors({ origin: origin, credentials: true }));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: false, cookie: { maxAge: 3600000 * 24 } }));
-
-app.get("/", (req, res) => {
- res.send("get success!");
-});
-
+app.get("/", (req, res) => res.send("get success!"));
 app.use("/upload", Upload);
 app.use("/api", indexRouter);
 app.use(errorHandler);
