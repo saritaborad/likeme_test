9cb436d768ac3d27214cf6d386332361097fba42
saritaborad <borad.sarita@gmail.com>
2023-11-27 10:19:52 +0530

    code changes

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ deploy_path="/var/www/piks/public_html/likeme_test"
 deploy_dir="$deploy_host:$deploy_path"
 ssh_key_path="$HOME/.ssh/id_rsa"  # Adjust the path based on your actual key location
 
-# Save the 'uploads' folder path
+
 uploads_folder="$deploy_path/backend/uploads"
 
 # Check if the 'uploads' folder exists on the server
