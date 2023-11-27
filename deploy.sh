4baf18947c38f55b1b2e04f08a151912a908348f
saritaborad <borad.sarita@gmail.com>
2023-11-27 11:41:24 +0530

    code changes

--- deploy.sh
+++ deploy.sh
@@ -15,7 +15,7 @@ if ssh -i $ssh_key_path $deploy_host "[ -d $uploads_folder ]"; then
   ssh -i $ssh_key_path $deploy_host "mv $uploads_folder $deploy_path/uploads_temp"
 fi
 
-# Pull the latest changes from the remote repository (assuming the branch is 'master')
+
 git pull origin master
 
 # Navigate to the backend directory on the server
