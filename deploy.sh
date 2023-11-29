4887d47d872254c202fe86f137ab610e941c0904
saritaborad <borad.sarita@gmail.com>
2023-11-29 17:29:17 +0530

    code changes

--- deploy.sh
+++ deploy.sh
@@ -23,12 +23,12 @@ fi
 git pull origin master
 
 # Navigate to the backend directory on the server
-ssh -i $ssh_key_path root@$deploy_host "cd $deploy_path/backend && npm install --legacy-peer-deps &&  pm2 restart likeme_test"
+ssh -i $ssh_key_path root@$deploy_host "cd $deploy_path/backend && npm install --legacy-peer-deps &&  /root/.nvm/versions/node/v19.7.0/bin/pm2 restart likeme_test"
 
 # Copy the build files to the deployment directory on the server
 scp -i $ssh_key_path -r build/* $deploy_dir
 
-# Check if the 'uploads' folder was moved to a temporary location
+
 ssh -i $ssh_key_path root@$deploy_host "[ -d $deploy_path/uploads_temp ]"
 if [ $? -eq 0 ]; then
   # If it was moved, restore it to its original location
