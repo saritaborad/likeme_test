f127d5b462724778c38b3d3d83a20d0aa4c00be9
saritaborad <borad.sarita@gmail.com>
2023-11-30 17:57:33 +0530

    code changes

--- backend/routes/User.js
+++ backend/routes/User.js
@@ -12,12 +12,12 @@ const router = express.Router();
 router.post("/multiUser", authenticate, multiUser);
 router.post("/verify_token", verifyToken);
 router.post("/logOut", logOut);
-// router.post("/fetchAllUser", authenticate, fetchAllUser);
+router.post("/fetchAllUser", authenticate, fetchAllUser);
 router.post("/AddCoin", authenticate, reqired("_id", "diamond"), AddCoin);
 router.post("/userblock", authenticate, reqired("_id", "is_block"), userblock);
 
 //  dashborad api
-// router.post("/fetchDashboardCount", authenticate, fetchDashboardCount);
+router.post("/fetchDashboardCount", authenticate, fetchDashboardCount);
 router.post("/fetchAgentDashboard", authenticate, fetchAgentDashboard);
 
 // -------- android api --------------
