082fd2102754aa6f365cf242b4009d638d2a708e
saritaborad <borad.sarita@gmail.com>
2023-11-30 17:39:57 +0530

    code changes

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ ssh_key_path="$HOME/.ssh/id_rsa"  # Adjust the path based on your actual key loc
 uploads_folder="$deploy_path/backend/uploads"
 
 # Ensure correct permissions for the SSH key
-chmod 600 $ssh_key_path
+chmod 600 $ssh_key_path 
 
 # Check if the 'uploads' folder exists on the server
 ssh -i $ssh_key_path root@$deploy_host "[ -d $uploads_folder ]"
@@ -22,17 +22,15 @@ fi
 # Pull the latest changes from the remote repository (assuming the branch is 'master')
 git fetch origin master
 
-changed_files_backend=$(git diff --name-only HEAD origin/master -- backend)
-echo "Changed files for backend: $changed_files_backend"
+# changed_files_backend=$(git diff --name-only HEAD origin/master -- backend)
+# echo "Changed files for backend: $changed_files_backend"
 
-# Copy only the changed files to the server for backend
-for file in $changed_files_backend; do
-  echo "Copying $file to server..."
-  rsync -avz -e "ssh -i $ssh_key_path" $file root@$deploy_path/backend
-done
-
-ssh -i $ssh_key_path root@$deploy_host "cd $deploy_path/backend && npm install &&  /root/.nvm/versions/node/v19.7.0/bin/pm2 restart likeme_test"
-
+# # Copy only the changed files to the server for backend
+# for file in $changed_files_backend; do
+#   echo "Copying $file to server..."
+#   rsync -avz -e "ssh -i $ssh_key_path" $file root@$deploy_path/backend                                              
+# done
+rsync -avz -e "ssh -i $ssh_key_path" backend/ root@$deploy_path/backend
 
 # ssh -i $ssh_key_path root@$deploy_host "cd $deploy_path/backend && mv build/* $deploy_path"
 # ssh -i $ssh_key_path root@$deploy_host "cd $deploy_path/backend && [ -d build ] && mv $deploy_path/backend/build/* $deploy_dir"
@@ -40,7 +38,7 @@ ssh -i $ssh_key_path root@$deploy_host "cd $deploy_path/backend && npm install &
 # rsync -r -e "ssh -i $ssh_key_path" backend/build/ root@$deploy_dir
 
 rsync -avz -e "ssh -i $ssh_key_path"  frontend/build/ root@$deploy_dir
-
+ssh -i $ssh_key_path root@$deploy_host "cd $deploy_path/backend && npm install && /root/.nvm/versions/node/v19.7.0/bin/pm2 restart likeme_test"
 
 
 ssh -i $ssh_key_path root@$deploy_host "[ -d $deploy_path/uploads_temp ]"
