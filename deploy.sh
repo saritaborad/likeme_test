564c023dc81cd2c6e0a0b245535c45d219c811f5
saritaborad <borad.sarita@gmail.com>
2023-11-30 11:12:27 +0530

    server changes

--- deploy.sh
+++ deploy.sh
@@ -29,9 +29,9 @@ ssh -i $ssh_key_path root@$deploy_host "cd $deploy_path/backend && npm install -
 # ssh -i $ssh_key_path root@$deploy_host "cd $deploy_path/backend && mv build/* $deploy_path"
 # ssh -i $ssh_key_path root@$deploy_host "cd $deploy_path/backend && [ -d build ] && mv $deploy_path/backend/build/* $deploy_dir"
 # ssh -i $ssh_key_path root@$deploy_host "cd $deploy_path/backend && [ -d build ] && mv build/* $deploy_dir"
-rsync -r -e "ssh -i $ssh_key_path" backend/build/ root@$deploy_dir
+# rsync -r -e "ssh -i $ssh_key_path" backend/build/ root@$deploy_dir
 
-# rsync -avz -e "ssh -i $ssh_key_path" --delete build/ $deploy_dir
+rsync -avz -e "ssh -i $ssh_key_path" --delete backend/build/ root@$deploy_dir
 
 
 
