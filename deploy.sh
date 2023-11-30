cd33ca4b7aaad5e825da2445f6d3049fb5fd1cc7
saritaborad <borad.sarita@gmail.com>
2023-11-30 12:46:20 +0530

    server changes

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,6 @@
 # Set the deployment directory on the server
 deploy_host="piks.in"
 deploy_path="/var/www/piks/public_html/likeme_test"
-deploy_dir="$deploy_host:$deploy_path"
 ssh_key_path="$HOME/.ssh/id_rsa"  # Adjust the path based on your actual key location
 
 # Save the 'uploads' folder path
@@ -22,23 +21,21 @@ fi
 # Pull the latest changes from the remote repository (assuming the branch is 'master')
 git pull origin master
 
+# Use git diff to get the list of changed files for backend
+changed_files_backend=$(git diff --name-only HEAD@{1} HEAD -- backend)
+# Copy only the changed files to the server for backend
+for file in $changed_files_backend; do
+  rsync -avz -e "ssh -i $ssh_key_path" $file root@$deploy_path/backend
+done
 
 ssh -i $ssh_key_path root@$deploy_host "cd $deploy_path/backend && npm install &&  /root/.nvm/versions/node/v19.7.0/bin/pm2 restart likeme_test"
 
-
-# ssh -i $ssh_key_path root@$deploy_host "cd $deploy_path/backend && mv build/* $deploy_path"
-# ssh -i $ssh_key_path root@$deploy_host "cd $deploy_path/backend && [ -d build ] && mv $deploy_path/backend/build/* $deploy_dir"
-# ssh -i $ssh_key_path root@$deploy_host "cd $deploy_path/backend && [ -d build ] && mv build/* $deploy_dir"
-# rsync -r -e "ssh -i $ssh_key_path" backend/build/ root@$deploy_dir
-
 rsync -avz -e "ssh -i $ssh_key_path"  frontend/build/ root@$deploy_dir
 
-
-
+# Restore the 'uploads' folder if it was moved
 ssh -i $ssh_key_path root@$deploy_host "[ -d $deploy_path/uploads_temp ]"
 if [ $? -eq 0 ]; then
-  # If it was moved, restore it to its original location
   ssh -i $ssh_key_path root@$deploy_host "mv $deploy_path/uploads_temp $uploads_folder"
 fi
+# Remove the build folder on the server (if needed)
 # ssh -i $ssh_key_path root@$deploy_host "rm -r $deploy_path/backend/build"
-
